f9fbfd01aa9be413d2c9c8e64ea2b25d9d559786
Gavrilo Adamovic <gavrilo.adamovic@gmail.com>
2024-11-03 09:30:51 +0100

    first commit

---
+++ src/main/java/com/subtitlescorrector/SubtitlescorrectorApplication.java
@@ -0,0 +1,13 @@
+package com.subtitlescorrector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubtitlescorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubtitlescorrectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subtitlescorrector/SubtitlescorrectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subtitlescorrector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubtitlescorrectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
