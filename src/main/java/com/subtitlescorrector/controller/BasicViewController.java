6df5a13ee8d986ec395032b0d135d60fe0ecdfb5
Gavrilo Adamovic <gavrilo.adamovic@gmail.com>
2025-01-10 16:12:16 +0100

    Added more client ip logging

--- src/main/java/com/subtitlescorrector/controller/BasicViewController.java
+++ src/main/java/com/subtitlescorrector/controller/BasicViewController.java
@@ -1,14 +1,23 @@
 package com.subtitlescorrector.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.subtitlescorrector.service.EmailServiceImpl;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class BasicViewController {
 	
-	@RequestMapping(path="/")
-	public String upload() {
+	Logger log = LoggerFactory.getLogger(BasicViewController.class);
 	
+	@RequestMapping(path="/")
+	public String upload(HttpServletRequest request) {
+		
+		log.info("Client ip: " + request.getRemoteAddr());
 		return "index";
 		
 	}
