46ccacdd61435cca6b12215c87e18cd49a0aa5fd
Gavrilo Adamovic <gavrilo.adamovic@gmail.com>
2025-01-25 13:44:59 +0100

    added more request mappings

--- src/main/java/com/subtitlescorrector/controller/BasicViewController.java
+++ src/main/java/com/subtitlescorrector/controller/BasicViewController.java
@@ -9,7 +9,7 @@ import com.subtitlescorrector.service.EmailServiceImpl;
 
 import jakarta.servlet.http.HttpServletRequest;
 
-//TODO: Implement Filter instead of controller
+//TODO: Implement Filter instead of controller for logging users
 
 @Controller
 public class BasicViewController {
@@ -17,9 +17,24 @@ public class BasicViewController {
 	Logger log = LoggerFactory.getLogger(BasicViewController.class);
 	
 	@RequestMapping(path="/")
-	public String upload(HttpServletRequest request) {
+	public String home(HttpServletRequest request) {
 		
 		log.info("Client ip: " + request.getRemoteAddr() + ". Request url: " + request.getRequestURL().toString());
+		return "index";
+		
+	}
+	
+	//for all return index because of client side rendering
+	@RequestMapping(path="terms-of-service")
+	public String termsOfService(HttpServletRequest request) {
+		log.info("terms-of-service");
+		return "index";
+		
+	}
+	
+	@RequestMapping(path="privacy-policy")
+	public String privacyPolicy(HttpServletRequest request) {
+		log.info("privacy-policy");
 		return "index";
 		
 	}
