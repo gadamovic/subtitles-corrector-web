8b54554ebf283f3ef25da754933cb799d829236a
Gavrilo Adamovic <gavrilo.adamovic@gmail.com>
2026-05-18 18:14:29 +0200

    Added counter initialization on startup

--- src/main/java/com/subtitlescorrector/adapters/out/PrometheusFeatureUsageMetricsExposerAdapter.java
+++ src/main/java/com/subtitlescorrector/adapters/out/PrometheusFeatureUsageMetricsExposerAdapter.java
@@ -6,17 +6,28 @@ import org.springframework.stereotype.Component;
 import com.subtitlescorrector.core.domain.metrics.Feature;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class PrometheusFeatureUsageMetricsExposerAdapter implements FeatureUsageMetricsExposerPort {
 
+	private static final String APP_FEATURE_USAGE_TOTAL = "app_feature_usage_total";
+	
 	@Autowired
 	private MeterRegistry registry;
 	
 	@Override
 	public void track(Feature feature) {
-		registry.counter("app_feature_usage_total", "feature", feature.getName())
+		registry.counter(APP_FEATURE_USAGE_TOTAL, "feature", feature.getName())
         .increment();
+	}
+	
+	@PostConstruct
+	public void init() {
+		for(Feature feature : Feature.values()) {
+			registry.counter(APP_FEATURE_USAGE_TOTAL, "feature", feature.getName())
+	        .increment(0);
+		}
 	}
 	
 }
